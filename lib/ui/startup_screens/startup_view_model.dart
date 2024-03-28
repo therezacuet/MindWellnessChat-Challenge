1c1e59b15c815c429753e317bc5210374b185333
Rezaul Islam <rejaulcuet88@gmail.com>
2024-03-28 23:53:07 +0600

    Implement startup logic to make sure proper screen landing

--- lib/ui/startup_screens/startup_view_model.dart
+++ lib/ui/startup_screens/startup_view_model.dart
@@ -2,14 +2,27 @@ import 'dart:async';
 
 import '../../app/routes/setup_routes.router.dart';
 import '../../base/custom_base_view_model.dart';
+import '../../models/user/user_basic_data_offline_model.dart';
 
 class StartUpViewModel extends CustomBaseViewModel {
   runStartupLogic() async {
-    // wait 2 second then navigate
-    Timer(
-        const Duration(
-            seconds: 2),
-            () => getNavigationService().clearStackAndShow(Routes.authView)
-    );
+    // await getAuthService().signOut();
+    bool isLoggedIn = await getAuthService().isUserLoggedIn();
+    UserBasicDataOfflineModel? _userBasicDataOfflineModel = await getDataManager().getUserBasicDataOfflineModel();
+    if (isLoggedIn && _userBasicDataOfflineModel != null) {
+
+      bool result = await getDataManager().getBackUpDataDownloadStatus();
+      print("RESULT : " + result.toString());
+      if(result){
+        getNavigationService().clearStackAndShow(Routes.mainScreenView);
+      }else{
+        //getNavigationService().clearStackAndShow(Routes.backUpFoundScreen);
+        print("backup found");
+      }
+
+    } else {
+      await getAuthService().logOut();
+      getNavigationService().clearStackAndShow(Routes.authView);
+    }
   }
 }
