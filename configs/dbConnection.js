b47ed6569a2954ec31b5b454f8c32da0f0297bf4
Rezaul Islam <rejaulcuet88@gmail.com>
2024-03-29 20:51:40 +0600

    Add http status code enum and fix typo on db connection

--- configs/dbConnection.js
+++ configs/dbConnection.js
@@ -2,13 +2,13 @@ import { connect } from 'mongoose';
 
 const dbUri = process.env.MONGO_DB_URL;
 
-export async function connectToDb(succes,failure) {
+export async function connectToDb(success,failure) {
     connect(dbUri, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => {
         console.log("Connection Successful");
-        succes();
+        success();
     }).catch((e) => {
         failure(e);
         console.log("No connection" + e);
