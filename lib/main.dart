75e3f44db3fbf2d86d6de7b826a0b4c73cc821cb
Rezaul Islam <rejaulcuet88@gmail.com>
2024-03-31 15:29:40 +0600

    Add launcher icon and notification icon

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ void main() {
       );
 
       await AwesomeNotifications().initialize(
-        null,
+        'resource://drawable/ic_stat_notification',
         [
           NotificationChannel(
             channelKey: 'basic_channel',
